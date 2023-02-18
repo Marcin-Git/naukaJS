d95265083ed2b7fafa83740b36ba20974bf8924e
Marcin Niedzwiecki <melasa4@vp.pl>
2023-02-18 17:47:25 +0100

    Funkcje STRZAŁKOWE

---
+++ funkce_arrow.js
@@ -0,0 +1,18 @@
+let sum = (a, b) => console.log(a+b);
+
+sum(2, 3);
+
+
+let iloczyn = (a, b) => {
+    console.log("Iloczyn liczb");
+    console.log(a*b);
+}
+iloczyn(4, 5);
+
+
+let square = a => console.log(a*a);
+square(9);
+
+
+let cosTam = () => console.log("Wyświetlanei coś tam");
+cosTam();
