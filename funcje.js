b59745bf48feb04b8d15c48f99c5b64354dcd4c8
Marcin Niedzwiecki <melasa4@vp.pl>
2023-02-18 17:38:06 +0100

    Funkcje proste

---
+++ funcje.js
@@ -0,0 +1,30 @@
+function hello() {
+    console.log("dzień dobrey")
+}
+
+hello();
+hello();
+hello();
+hello();
+hello();
+
+function suma(a, b) {
+    console.log(a + b)
+}
+
+suma(1, 2);
+suma(3, 4);
+suma(5, 6);
+suma(7, 8);
+suma(9, 9);
+
+
+function iloczyn(a, b) {
+    return (a * b);
+};
+
+console.log(iloczyn(1, 2));
+console.log(iloczyn(3, 4));
+console.log(iloczyn(5, 6));
+console.log(iloczyn(7, 8));
+console.log(iloczyn(9, 9));
